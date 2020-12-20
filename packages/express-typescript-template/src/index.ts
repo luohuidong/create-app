0a44d0ee4d8124e0f8e39a03814d8a2c6f57c340
luohuidong <luohuidong01@126.com>
2020-12-21 07:16:33 +0800

    feat: 新增 middleware 的加载逻辑

--- packages/express-typescript-template/src/index.ts
+++ packages/express-typescript-template/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
+import middleware from "./middleware";
 import router from "./router";
 
 const app = express();
 const port = 8000;
 
-router(app);
+middleware(app).then(() => {
+  router(app);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- packages/express-typescript-template/src/router/user.ts
+++ packages/express-typescript-template/src/router/user.ts
@@ -2,11 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log("Time: ", Date.now());
-  next();
-});
 // define the home page route
 router.get("/", function (req, res) {
   res.send("User get");

3b83f08427de5247c080fa8113939abf98e5a6a0
luohuidong <luohuidong01@126.com>
2021-01-24 11:30:02 +0800

    build: 更新 packages

--- packages/express-typescript-template/src/middleware/bodyParser.ts
+++ packages/express-typescript-template/src/middleware/bodyParser.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+import * as bodyParser from "body-parser";
 
 export default function (app: express.Application): void {
   app.use(bodyParser.urlencoded({ extended: false }));

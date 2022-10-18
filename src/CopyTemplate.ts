aa6366636f0a1cc49231397b379236f30e739ca5
luohuidong <luohuidong01@126.com>
2022-10-18 11:46:29 +0800

    refactor: add koa template

--- src/CopyTemplate.ts
+++ src/CopyTemplate.ts
@@ -14,6 +14,7 @@ export default class CopyTemplate {
   private _templateNames = [
     "template-node-typescript",
     "template-express-typescript",
+    "template-koa",
     "template-koa-mongodb",
     "template-hexo-blog",
   ];

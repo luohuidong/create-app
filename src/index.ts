8b8b8f6fb4b79da2e4dd67faab6060f8b30c75aa
luohuidong <luohuidong01@126.com>
2023-08-11 14:44:18 +0800

    feat: the CLI now supports initializing the project by passing options

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
+import { program } from "commander";
 
 import CopyTemplate from "./CopyTemplate.js";
 import { errMsg } from "./errMsg.js";
 import fs from "node:fs";
 import path from "node:path";
 
-try {
-  const answers = await inquirer.prompt<{
-    projectName: string;
-    template: string;
-  }>([
-    {
-      name: "projectName",
-      type: "input",
-      message: "project name:",
-    },
-    {
-      name: "template",
-      type: "list",
-      message: "template:",
-      choices: fs
-        .readdirSync(new URL("../tmp", import.meta.url))
-        .map((filename) => path.basename(filename, ".tar.gz")),
-    },
-  ]);
+program.name("create-app").description("CLI for creating a JavaScript new app");
+program
+  .option("-t, --template <templateName>", "template name")
+  .option("-n, --name <projectName>", "project name")
+  .parse();
 
-  const { template, projectName } = answers;
+const { template, name } = program.opts();
 
-  const copyTemplate = new CopyTemplate(template, projectName);
-  copyTemplate.copy();
-} catch (err) {
-  console.log(errMsg(err));
+if (template && name) {
+  const copyTemplate = new CopyTemplate(template, name);
+  await copyTemplate.copy();
+} else {
+  try {
+    const answers = await inquirer.prompt<{
+      projectName: string;
+      template: string;
+    }>([
+      {
+        name: "projectName",
+        type: "input",
+        message: "project name:",
+      },
+      {
+        name: "template",
+        type: "list",
+        message: "template:",
+        choices: fs
+          .readdirSync(new URL("../tmp", import.meta.url))
+          .map((filename) => path.basename(filename, ".tar.gz")),
+      },
+    ]);
+
+    const { template, projectName } = answers;
+
+    const copyTemplate = new CopyTemplate(template, projectName);
+    await copyTemplate.copy();
+  } catch (err) {
+    console.log(errMsg(err));
+  }
 }

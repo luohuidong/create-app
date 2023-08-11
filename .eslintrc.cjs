a48b0c589dd3155e3ba73201ebc34c6776ccb5ab
luohuidong <luohuidong01@126.com>
2023-08-11 14:37:34 +0800

    fix: resolve the issue of eslint not working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,5 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 };

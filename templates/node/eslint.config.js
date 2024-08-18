507d3fddf1d2b99961dbb3c64d6ab1827b175ba9
Luo Huidong <luohuidong01@126.com>
2024-08-18 20:44:45 +0800

    modify templates

--- templates/node/eslint.config.js
+++ templates/node/eslint.config.js
@@ -4,6 +4,9 @@ import tseslint from "typescript-eslint";
 
 export default [
   { files: ["src/**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignore: ["node_modules/", "dist/", "coverage/"],
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
